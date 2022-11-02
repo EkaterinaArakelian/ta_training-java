943c16ce636d2f30f4097eec384c90c620e79145
Ekaterina Arakelian <gramotovae@gmail.com>
2022-11-02 22:52:26 +0100

    Aircompany

--- Java/src/main/java/Planes/experimentalPlane.java
+++ Java/src/main/java/Planes/experimentalPlane.java
@@ -1,16 +1,17 @@
-package Planes;
+package planes;
 
 import models.ClassificationLevel;
 import models.ExperimentalTypes;
 
-public class experimentalPlane extends Plane{
+public class ExperimentalPlane extends Plane{
 
-    private ExperimentalTypes type;
+    private ExperimentalTypes experimentalType;
     private ClassificationLevel classificationLevel;
 
-    public experimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity,
+            ExperimentalTypes type, ClassificationLevel classificationLevel) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-        this.type = type;
+        this.experimentalType = type;
         this.classificationLevel = classificationLevel;
     }
 
@@ -23,8 +24,8 @@ public class experimentalPlane extends Plane{
     }
 
     @Override
-    public boolean equals(Object o) {
-        return super.equals(o);
+    public boolean equals(Object object) {
+        return super.equals(object);
     }
 
     @Override
@@ -34,8 +35,6 @@ public class experimentalPlane extends Plane{
 
     @Override
     public String toString() {
-        return "experimentalPlane{" +
-                "model='" + model + '\'' +
-                '}';
+        return "experimentalPlane{" + "model='" + model + '\'' + '}';
     }
 }
